2b154f7500694bf449037d73490e9d262a2fd066
Luis Otávio <131190540+luisotavio2023@users.noreply.github.com>
2024-04-04 17:26:22 -0300

    Add files via upload

---
+++ livrosRouter.js
@@ -0,0 +1,197 @@
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+const express = require('express');
+const app = express();
+
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://aulaprogmult-default-rtdb.firebaseio.com"
+});
+const db = admin.database();
+
+function criarTabela(dados) {
+    let tabela = `<table class="table table-striped zebrado">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome do Livro</th>
+                            <th>Editora</th>
+                            <th>Gênero</th>
+                            <th>Valor</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>`;
+    for (let chave in dados) {
+        tabela += `<tr>
+                        <td>${chave}</td>
+                        <td>${dados[chave].nome}</td>
+                        <td>${dados[chave].editora}</td>
+                        <td>${dados[chave].genero}</td>
+                        <td>${dados[chave].valor}</td>
+                        <td>
+                            <a class="btn btn-outline-warning" href="/livros/editar/${chave}">Alterar</a>
+                        </td>
+                        <td>
+                            <a class="btn btn-outline-danger" href="/livros/excluir/${chave}">Excluir</a>
+                        </td>
+                    </tr>`;
+    }            
+    tabela += `</tbody >
+            </table > `;
+    return tabela;
+}
+
+
+// Rota da página que exibe os livros registrados no banco de dados
+app.get('/', (req, res) => {
+    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
+        fs.readFile('src/rodape.html', (e, rodape) => {
+            fs.readFile('src/livros/livros.html', (e, dados) => {
+                let tabela = "";
+                const docLivros = db.ref("livros");
+                docLivros.once("value", function(snapshot){
+                    tabela = criarTabela(snapshot.val());
+                    dados = dados.toString().replace("{tabela}", tabela);
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                });
+            });
+        });
+    });
+});
+
+// Rota da página para abrir formulário para inserir um novo registro de livro
+app.get('/novo', (req, res) => {
+    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
+        fs.readFile('src/rodape.html', (e, rodape) => {
+            fs.readFile('src/livros/novo_livro.html', (e, dados) => {
+                let id = req.params.id;
+                const docLivro = db.ref("Livros/"+id);
+                docLivro.once("value", function(snapshot){
+                    let nome = snapshot.val().nome;
+                    let genero = snapshot.val().genero;
+                    let editora = snapshot.val().editora;
+                    let valor = snapshot.val().valor;
+                    dados = dados.toString().replace("{nome}", nome);
+                    dados = dados.toString().replace("{genero}", genero);
+                    dados = dados.toString().replace("{editora}", editora);
+                    dados = dados.toString().replace("{valor}", valor);
+                    dados = dados.toString().replace("{id}", id);
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                })
+            });
+        });
+    });
+});
+
+// Rota da página inserir um novo registro de livro
+app.post('/novo', urlencodedParser, (req, res) => {
+    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
+        fs.readFile('src/rodape.html', (e, rodape) => {
+            fs.readFile('src/livros/livros.html', (e, dados) => {
+                let mensagem = "";
+                try{
+                    const docLivro = db.ref("livros").push();
+                    const livro = {
+                        nome: req.body.nome,
+                        editora: req.body.editora,
+                        genero: req.body.genero,
+                        valor: req.body.valor
+                    };
+                    docLivro.set(livro);
+                    mensagem = "Livro inserido com sucesso!";
+                }catch(e){
+                    mensagem = "Erro ao inserir o livro!";
+                }
+                dados = dados.toString().replace("{mensagem}", mensagem);
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(cabecalho + dados + rodape);
+                res.end();
+            });
+        });
+    });
+});
+
+// Rota da página para abrir formuário para editar os dados de um registro de livro
+app.get('/editar/:id', (req, res) => {
+    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
+        fs.readFile('src/rodape.html', (e, rodape) => {
+            fs.readFile('src/livros/editar_livro.html', (e, dados) => {
+                let id = req.params.id;
+                const docLivro = db.ref("livros/"+id);
+                docLivro.once("value", function(snapshot){
+                    let nome = snapshot.val().nome;
+                    let genero = snapshot.val().genero;
+                    let editora = snapshot.val().editora;
+                    let valor = snapshot.val().valor;
+                    dados = dados.toString().replace("{nome}", nome);
+                    dados = dados.toString().replace("{genero}", genero);
+                    dados = dados.toString().replace("{editora}", editora);
+                    dados = dados.toString().replace("{valor}", valor);
+                    dados = dados.toString().replace("{id}", id);
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                });
+            });
+        });
+    });
+});
+
+// Rota da página para editar os dados de um registro de livro
+app.post('/editar', urlencodedParser, (req, res) => {
+    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
+        fs.readFile('src/rodape.html', (e, rodape) => {
+            fs.readFile('src/livros/livros.html', (e, dados) => {
+                dados = dados.toString().replace("{mensagem}", "Alterado!");
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(cabecalho + dados + rodape);
+                res.end();
+            });
+        });
+    });
+});
+
+// Rota da página para abrir formulário para excluir um registro de um livro
+app.get('/excluir/:id', (req, res) => {
+    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
+        fs.readFile('src/rodape.html', (e, rodape) => {
+            fs.readFile('src/livros/excluir_livro.html', (e, dados) => {
+                let id = req.params.id;
+                const docLivro = db.ref("livros/"+id);
+                docLivro.once("value", function(snapshot){
+                    let nome = snapshot.val().nome;
+                    let genero = snapshot.val().genero;
+                    let editora = snapshot.val().editora;
+                    let valor = snapshot.val().valor;
+                    dados = dados.toString().replace("{nome}", nome);
+                    dados = dados.toString().replace("{genero}", genero);
+                    dados = dados.toString().replace("{editora}", editora);
+                    dados = dados.toString().replace("{valor}", valor);
+                    dados = dados.toString().replace("{id}", id);
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                });
+            });
+        });
+    });
+});
+
+// Rota da página para excluir um registro de um livro
+app.post('/excluir', urlencodedParser, (req, res) => {
+    let id = req.body.id;
+    const docLivro = db.ref("livros/"+id);
+    docLivro.remove().then();
+    res.redirect("/livros");
+});
+
+module.exports = app;
